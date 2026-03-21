f3717402503b82c9af1f0af6001269ceb35a8b5e
oofbomb <76566861+meatballsong1@users.noreply.github.com>
2026-03-20 20:20:09 -0500

    v2.5.7: welcome to 2.5.7!

--- content.js
+++ content.js
@@ -1635,7 +1635,7 @@ edStop = function() {
 // -- UPDATE NOTIFIER ------------------------------------------------------
 // =========================================================================
 
-var VEIL_CURRENT_VERSION = '2.5.6';
+var VEIL_CURRENT_VERSION = '2.5.7';
 var UPDATE_CHECK_URL = 'https://raw.githubusercontent.com/meatballsong1/po-extension/main/version.json?t=';
 
 
